6fee1f4f444d16626306d69238a8fe988091fd9a
pritam <averagestudent70@gmail.com>
2026-08-08 20:19:31 +0530

    Handle X11 Events without Blocking

--- main.cpp
+++ main.cpp
@@ -1,55 +1,64 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
-#include <cstdio>
+#include <print>
+
+struct WindowProperties {
+    unsigned width = 0, height = 0;
+    unsigned minWidth = 0, minHeight = 0;
+    const char *title = nullptr;
+};
 
 int main() {
-    Display *display = XOpenDisplay(nullptr);
+    constexpr WindowProperties window_properties {
+        .width = 1280, .height = 720,
+        .minWidth = 360, .minHeight = 640,
+        .title = "X11 Window",
+    };
 
-    constexpr unsigned int windowWidth = 800, windowHeight = 600;
-    const char *windowTitle = "X11 Window";
+    Display *display = XOpenDisplay(nullptr);
 
     constexpr int attributeMask = CWBackPixel | CWEventMask;
     XSetWindowAttributes windowAttributes = {
-        .background_pixel = 0x00aade87,
-        .event_mask = StructureNotifyMask | KeyPressMask | KeyReleaseMask | ExposureMask
+        .background_pixel = WhitePixel(display, DefaultScreen(display)),
+        .event_mask = KeyPressMask | KeyReleaseMask
+        // TODO: look into the usefulness of StructureNotifyMask and ExposureMask
+    };
+    XSizeHints sizeHint = {
+        .flags = PMinSize, // TODO: look into min_(max)aspect
+        .min_width = static_cast<int>(window_properties.minWidth),
+        .min_height = static_cast<int>(window_properties.minHeight),
     };
 
-    Window window = XCreateWindow(
+    const Window window = XCreateWindow(
         display, DefaultRootWindow(display),
-        0, 0, windowWidth, windowHeight,
+        0, 0, window_properties.width, window_properties.height,
         0, CopyFromParent, CopyFromParent, CopyFromParent,
         attributeMask, &windowAttributes);
 
-    XMapWindow(display, window);
-    XStoreName(display, window, windowTitle);
+    XSetStandardProperties(display, window,
+        window_properties.title, nullptr, None, nullptr, 0, &sizeHint);
 
     Atom WM_DELETE_WINDOW = XInternAtom(display, "WM_DELETE_WINDOW", false);
-    if (!XSetWMProtocols(display, window, &WM_DELETE_WINDOW, 1)) {
-        std::printf("XSetWMProtocols failed: Couldn't set WM_DELETE_WINDOW property\n");
-        // std::exit(1);
-    }
+    if (!XSetWMProtocols(display, window, &WM_DELETE_WINDOW, 1))
+        std::println(stderr,"%s", "XSetWMProtocols failed: Couldn't set WM_DELETE_WINDOW property");
+
+    XMapWindow(display, window);
+
     bool isWindowOpen = true;
     while (isWindowOpen) {
         XEvent generalEvent;
-        XNextEvent(display, &generalEvent);
+        while (XPending(display) > 0) {
+            XNextEvent(display, &generalEvent);
 
-        switch (generalEvent.type) {
-            case Expose:
-                std::printf("expose\n");
-                break;
-            case KeyPress: {
-                std::printf("key press\n");
-                auto *keyEvent = reinterpret_cast<XKeyPressedEvent *>(&generalEvent);
-                if (keyEvent->keycode == XKeysymToKeycode(display, XK_Escape))
-                    isWindowOpen = false;
-                break;
-            }
-            case ClientMessage: {
-                auto *event = reinterpret_cast<XClientMessageEvent *>(&generalEvent);
-                std::printf("client message\n");
-                if (static_cast<Atom>(event->data.l[0]) == WM_DELETE_WINDOW)
-                    isWindowOpen = false;
-                break;
+            switch (generalEvent.type) {
+                case KeyPress:
+                    if (generalEvent.xkey.keycode == XKeysymToKeycode(display, XK_Escape))
+                        isWindowOpen = false;
+                    break;
+                case ClientMessage:
+                    if (static_cast<Atom>(generalEvent.xclient.data.l[0]) == WM_DELETE_WINDOW)
+                        isWindowOpen = false;
+                    break;
             }
         }
     }
